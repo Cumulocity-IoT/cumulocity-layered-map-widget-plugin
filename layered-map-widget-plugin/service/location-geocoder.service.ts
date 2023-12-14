acf2f93ea8a71e1f7fc5ed0cf20b4b76d1aafc77
Hendrik Näther <hendrik.naether@softwareag.com>
2023-12-14 10:58:45 +0100

    Minor changes

--- layered-map-widget-plugin/service/location-geocoder.service.ts
+++ layered-map-widget-plugin/service/location-geocoder.service.ts
@@ -5,7 +5,7 @@ import { isEmpty } from 'lodash';
 
 @Injectable()
 export class LocationGeocoderService {
-  mapKey = 'MgOKczqMYTkXK5jiMgEYGvjnTHf562mA';
+  mapKey = '';
   geoCodeSearchUrl = `http://www.mapquestapi.com/geocoding/v1/address`;
 
   async geoCode(address: string): Promise<LatLng | null> {
